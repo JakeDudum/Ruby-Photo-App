539ccbcc624e1d4a42debdf0a8cf2c3dadfb97d4
Jake Dudum <jakedudum@gmail.com>
2022-05-06 15:21:13 -0700

    Configured app for email functionality. Tried to resolve alert dismiss error with bootstrap, but still no success.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {     
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials.GMAIL_USERNAME,     
+    :password => Rails.application.credentials.GMAIL_PASSWORD,     
+    :domain => 'gmail.com',   
+    :enable_starttls_auto => true 
+  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "immense-sands-01561.herokuapp.com" , protocol: 'https' }   
+  
 end
