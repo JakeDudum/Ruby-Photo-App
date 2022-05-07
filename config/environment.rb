10e0c045f3a1703725cd6ae82c76f363223fbab8
Jake Dudum <jakedudum@gmail.com>
2022-05-07 14:57:39 -0700

    Modified environment.rb for production.

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.gmail.com',
     :port => '587',
     :authentication => :plain,
-    :user_name => Rails.application.credentials.GMAIL_USERNAME,     
-    :password => Rails.application.credentials.GMAIL_PASSWORD,     
+    :user_name => ENV['GMAIL_USERNAME'],     
+    :password => ENV['GMAIL_PASSWORD'],     
     :domain => 'gmail.com',   
     :enable_starttls_auto => true 
   }
